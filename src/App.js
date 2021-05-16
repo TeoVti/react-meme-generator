a551cbd6e42a53b1bb332e88bc2247943b980eed
Teofil-Ioan Vartolomei <teo.vty@gmail.com>
2021-05-16 16:45:14 +0200

    Added download button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import { useState } from 'react';
+import ImageComponent from './imageComponent';
 
 function App() {
   const [topText, setTopText] = useState('memes');
   const [bottomText, setBottomText] = useState('memes everywhere');
   const [img, setImg] = useState('buzz');
   const imgUrl = `https://api.memegen.link/images/${img}/${topText}/${bottomText}.png`;
+  const options = {};
+  const [realUrl, SetRealUrl] = useState();
 
   const handleOnChangeTop = (event) => {
     setTopText(event.currentTarget.value);
@@ -17,19 +20,73 @@ function App() {
     setImg(event.currentTarget.value);
   };
 
+  const displayData = () => {
+    SetRealUrl(imgUrl);
+  };
+
+  const dwnl = () => {
+    fetch(imgUrl, options)
+      .then((res) => res.blob())
+      .then((blob) => {
+        downloadBlob(blob, 'meme.png');
+      });
+
+    function downloadBlob(blob, name = 'meme.png') {
+      // Convert your blob into a Blob URL (a special url that points to an object in the browser's memory)
+      const blobUrl = URL.createObjectURL(blob);
+
+      // Create a link element
+      const link = document.createElement('a');
+
+      // Set link's href to point to the Blob URL
+      link.href = blobUrl;
+      link.download = name;
+
+      // Append link to the body
+      document.body.appendChild(link);
+
+      // Dispatch click event on the link
+      // This is necessary as link.click() does not work on the latest firefox
+      link.dispatchEvent(
+        new MouseEvent('click', {
+          bubbles: true,
+          cancelable: true,
+          view: window,
+        }),
+      );
+      // Remove link from body
+      document.body.removeChild(link);
+    }
+  };
+
   return (
     <div className="content">
-      <input value={topText} type="text" onChange={handleOnChangeTop}></input>
-      <input
-        value={bottomText}
-        type="text"
-        onChange={handleOnChangeBottom}
-      ></input>
-      <input value={img} type="text" onChange={handleOnChangeImg}></input>
-      <div>
-        <a href={imgUrl} download="jhg">
-          <img src={imgUrl} alt={img} />
-        </a>
+      <div className="column">
+        <ImageComponent realUrl={realUrl} />
+      </div>
+      <div className="column">
+        <div className="form">
+          <input
+            value={topText}
+            type="text"
+            onChange={handleOnChangeTop}
+          ></input>
+          <input
+            value={bottomText}
+            type="text"
+            onChange={handleOnChangeBottom}
+          ></input>
+          <form>
+            <select onChange={handleOnChangeImg}>
+              <option value="afraid">Afraid</option>
+              <option value="ackbar">Ackbar</option>
+              <option value="aag">Aag</option>
+              <option value="buzz">Buzz</option>
+            </select>
+          </form>
+          <button onClick={displayData}>Click</button>
+          <button onClick={dwnl}>Download</button>
+        </div>
       </div>
     </div>
   );
---
+++ src/imageComponent.js
@@ -0,0 +1,8 @@
+function ImageComponent(props) {
+  return (
+    <div>
+      <img src={props.realUrl} />
+    </div>
+  );
+}
+export default ImageComponent;
