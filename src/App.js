96c1d9de7a946888bbf16eb2264cf1ee486ab692
Teofil-Ioan Vartolomei <teo.vty@gmail.com>
2021-05-17 11:43:57 +0200

    Fixed bugs

--- src/App.js
+++ src/App.js
@@ -24,39 +24,38 @@ function App() {
     SetRealUrl(imgUrl);
   };
 
+  function downloadBlob(blob, name = 'meme.png') {
+    // Convert your blob into a Blob URL (a special url that points to an object in the browser's memory)
+    const blobUrl = URL.createObjectURL(blob);
+
+    // Create a link element
+    const link = document.createElement('a');
+
+    // Set link's href to point to the Blob URL
+    link.href = blobUrl;
+    link.download = name;
+
+    // Append link to the body
+    document.body.appendChild(link);
+
+    // Dispatch click event on the link
+    // This is necessary as link.click() does not work on the latest firefox
+    link.dispatchEvent(
+      new MouseEvent('click', {
+        bubbles: true,
+        cancelable: true,
+        view: window,
+      }),
+    );
+    // Remove link from body
+    document.body.removeChild(link);
+  }
   const dwnl = () => {
     fetch(imgUrl, options)
       .then((res) => res.blob())
       .then((blob) => {
         downloadBlob(blob, 'meme.png');
       });
-
-    function downloadBlob(blob, name = 'meme.png') {
-      // Convert your blob into a Blob URL (a special url that points to an object in the browser's memory)
-      const blobUrl = URL.createObjectURL(blob);
-
-      // Create a link element
-      const link = document.createElement('a');
-
-      // Set link's href to point to the Blob URL
-      link.href = blobUrl;
-      link.download = name;
-
-      // Append link to the body
-      document.body.appendChild(link);
-
-      // Dispatch click event on the link
-      // This is necessary as link.click() does not work on the latest firefox
-      link.dispatchEvent(
-        new MouseEvent('click', {
-          bubbles: true,
-          cancelable: true,
-          view: window,
-        }),
-      );
-      // Remove link from body
-      document.body.removeChild(link);
-    }
   };
 
   return (
@@ -72,13 +71,13 @@ function App() {
               value={topText}
               type="text"
               onChange={handleOnChangeTop}
-            ></input>
+            />
             <input
               placeholder="Type bottom text"
               value={bottomText}
               type="text"
               onChange={handleOnChangeBottom}
-            ></input>
+            />
             <form>
               <select onChange={handleOnChangeImg}>
                 <option selected="true" value="Background image" disabled>
